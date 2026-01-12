abfeba04615de7ed0c98f9565e41d622ff67dfab
Vishal Row Mysore <visrow@gmail.com>
2026-01-11 19:38:00 -0500

    hydration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,11 @@
 import { ApplicationConfig } from '@angular/core';  
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideA2UI, DEFAULT_CATALOG } from '@a2ui/angular';  
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';  
 import { theme } from './theme';
 import { a2aInterceptor } from './a2a.interceptor';  
   
 export const appConfig: ApplicationConfig = {  
-  providers: [  
-    provideClientHydration(withEventReplay()),
+providers: [  
     provideHttpClient(withInterceptors([a2aInterceptor])),
     provideA2UI({  
       catalog: DEFAULT_CATALOG,  
